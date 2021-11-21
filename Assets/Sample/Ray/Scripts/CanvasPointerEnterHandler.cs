5c14304fc642f557db10538b73968c6d1303b4e7
Kyohei <kurokenken0310@gmail.com>
2021-11-21 17:08:55 +0900

    vr ui

---
+++ Assets/Sample/Ray/Scripts/CanvasPointerEnterHandler.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+ using UnityEngine;
+ using UnityEngine.EventSystems;
+
+ public class CanvasPointerEnterHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+ {
+     [SerializeField] private OVRInputModule _ovrInputModule;
+     [SerializeField] private OVRRaycaster _ovrRaycaster;
+
+     private bool _isEnter = false;
+     
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+         _isEnter = true;
+         Debug.Log($"PointerEnter!!{eventData.position.ToString()}");
+     }
+
+     public void Update()
+     {
+         if (!_isEnter)
+         {
+             return;
+         }
+     }
+
+     public void OnPointerExit(PointerEventData eventData)
+     {
+         _isEnter = false;
+         Debug.Log("PointerExit!!");
+     }
+ }
